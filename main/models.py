f60e3a973339192c71cfe4e30d311ecb6b149878
laizabeatriz <laizabeatrizsantana@gmail.com>
2022-10-25 19:05:30 -0300

    Criação de Model (pedidos)

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Pedido (models.Model): 
+    descricao = models.CharField(max_length=200)
+    data = models.DateTimeField() 
